d1e080a4a9cdc7a6cb9c8ff868bc5dffb3e58ab4
Ben Xiang <63603790+jonben3215@users.noreply.github.com>
2023-11-26 12:41:59 -0700

    Update

---
+++ JS/demo.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var myButton = document.getElementById("Demo");
+    myButton.addEventListener("click", function() {
+        window.open("https://www.figma.com/proto/T7xEpwxdN5bHkfovQrjGIt/ParkPro?node-id=1-2");
+    });
+});
